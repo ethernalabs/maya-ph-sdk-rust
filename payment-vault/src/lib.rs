fbe03d2141e613742e7d545e4fa0afec520991e4
Norman Santiago <nsantiago2719@gmail.com>
2023-04-04 13:04:37 +0800

    updated create_payment_token with test

--- payment-vault/src/lib.rs
+++ payment-vault/src/lib.rs
@@ -1,40 +1,87 @@
-use serde::{Deserialize, Serialize};
-use maya_client_sdk::MayaClient;
+use base64::{
+    alphabet,
+    engine::{self, general_purpose},
+    Engine as _,
+};
+use reqwest::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    Client, Response,
+};
+use serde::Serialize;
 
 pub mod payment_gateway {
-  use reqwest::header::{AUTHORIZATION, ACCEPT, CONTENT_TYPE};
+    const CUSTOM_ENGINE: engine::GeneralPurpose =
+        engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
-  use super::*;
+    use super::*;
 
-  
-  #[derive(Debug, Serialize, Deserialize)]
-  #[allow(non_snake_case)]
-  pub struct Card {
-      pub number: String,
-      pub expYear: String,
-      pub expMonth: String,
-      pub cvv: String,
-  }
-  
-  #[tokio::main]
-  pub async fn create_payment_token(
-      base_url: String,
-      card: Card,
-  ) -> Result<reqwest::Response, reqwest::Error> {
-    
-    let token: String = MayaClient::new("", "").token;
-    let client = reqwest::Client::new();
-    let path: String = format!("{}{}", &base_url, "/payments/v1/payment-tokens");
+    #[derive(Debug, Serialize)]
+    #[allow(non_snake_case)]
+    pub struct CardDetails {
+        pub number: String,
+        pub expYear: String,
+        pub expMonth: String,
+        pub cvc: String,
+    }
 
-    
-    let res = client.post(&path)
-                              .json(&card)
-                              .header(CONTENT_TYPE, "application/json")
-                              .header(AUTHORIZATION, &token)
-                              .header(ACCEPT, "application/json")
-                              .send().await?;
+    #[derive(Debug, Serialize)]
+    pub struct Card {
+        pub card: CardDetails,
+    }
 
-    println!("{:#?}", res);
-    Ok(res)
-  }  
+    pub struct MayaClient {
+        client: Client,
+        url_domain: String,
+        auth_header: String,
+    }
+
+    impl MayaClient {
+        pub fn new(username: String, password: String, url_domain: Option<String>) -> Self {
+            let auth_header = Self::generate_auth_header_value(&username, &password);
+            let client = Client::new();
+
+            let domain = match url_domain {
+                Some(v) => v,
+                None => "pg-sandbox.paymaya.com".to_string(),
+            };
+            MayaClient {
+                client,
+                auth_header,
+                url_domain: domain,
+            }
+        }
+
+        fn generate_auth_header_value(username: &str, password: &str) -> String {
+            let mut token = String::new();
+            let token_format = format!("{}:{}", username, password);
+
+            CUSTOM_ENGINE.encode_string(&token_format.as_bytes(), &mut token);
+
+            return token;
+        }
+
+        pub async fn create_payment_token(
+            &self,
+            card_details: CardDetails,
+        ) -> Result<Response, Box<dyn std::error::Error>> {
+            let url = format!("https://{}/payments/v1/payment-tokens", self.url_domain);
+            let auth = format!("Basic {}", &self.auth_header);
+            let request_body = Card { card: card_details };
+
+            let response = self
+                .client
+                .post(&url)
+                .header(AUTHORIZATION, auth)
+                .header(ACCEPT, "application/json")
+                .header(CONTENT_TYPE, "application/json")
+                .json(&request_body)
+                .send()
+                .await?
+                .error_for_status()?;
+
+            Ok(response)
+        }
+
+        pub async fn create_payment() -> () {}
+    }
 }
