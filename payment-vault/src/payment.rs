c2198bc86f1c4b08074f7ceecbe6c3bf9dbf6450
norman santiago <nsantiago2719@gmail.com>
2023-08-10 00:59:14 +0000

    added traits for buyer struct

--- payment-vault/src/payment.rs
+++ payment-vault/src/payment.rs
@@ -1,5 +1,5 @@
 use super::*;
-
+pub mod payment_gateway;
 /// Details for creating payment token
 #[derive(Debug, Serialize)]
 #[allow(non_snake_case)]
@@ -21,6 +21,35 @@ pub enum ShippingType {
     SD,
 }
 
+pub struct ContactDetails {
+    pub phone: Option<String>,
+    pub email: Option<String>
+}
+
+pub enum Sex {
+    "M",
+    "F"
+}
+
+#[derive(Debug)]
+pub struct Buyer<T = Sex> {
+    pub firstName: String,
+    pub middleName: Option<String>,
+    pub lastName: String,
+    pub birthday: String, /// Birthday in ISO 8601 / RFC 3339 full-date format.
+    pub customerSince: String, /// Date in ISO 8601 / RFC 3339 full-date format 
+    pub sex: T,
+    pub contact: ContactDetails
+
+}
+
+#[derive(Debug)]
+pub struct RedirectUrl {
+    pub success: Option<String>,
+    pub failure: Option<String>,
+    pub cancel: Option<String>
+}
+
 #[derive(Debug, Serialize)]
 #[allow(non_snake_case)]
 pub struct Address<T = ShippingType> {
@@ -29,8 +58,7 @@ pub struct Address<T = ShippingType> {
     pub city: Option<String>,
     pub state: Option<String>,
     pub zipCode: Option<String>,
-    // ISO 3166 aplha-2 notion of the country
-    pub countryCode: String,
+    pub countryCode: String, /// ISO 3166 aplha-2 notion of the country
     pub firstName: Option<String>,
     pub middleName: Option<String>,
     pub lastName: Option<String>,
@@ -39,91 +67,3 @@ pub struct Address<T = ShippingType> {
     pub shippingType: T,
 }
 
-#[async_trait]
-pub trait PaymentGateway {
-    /// Returns a response that contains the paymentTokenId to be use for furhter transaction
-    /// to the API endpoint else an std::error:Error
-    async fn create_payment_token(
-        &self,
-        card_details: CardDetails,
-    ) -> Result<Response, Box<dyn std::error::Error>>;
-
-    /// Returns a respons that contains the details of the payment transaction
-    async fn create_payment(&self) -> Result<Response, Box<dyn std::error::Error>>;
-}
-
-impl Address {
-    fn new(&self) -> Self {
-        todo!()
-    }
-}
-
-/// Implement the PaymentGateway trait to MayaClient
-#[async_trait]
-impl PaymentGateway for MayaClient {
-    /// Returns a Result response containing the Response or a std::error::Error when
-    /// request is made to the API endpoint of payment-token
-    ///
-    /// It takes a parameter card_details with a type of CardDetails struct
-    /// which then will be sent to the API endpoint of the payment token. The auth_header and
-    /// url_domain are taken from self and used for generating the full endpoint url and
-    /// authorization header needed to authenticate to the end point
-    ///
-    /// ```rust,no_run
-    /// use maya_client_sdk::MayaClient;
-    /// use maya_payment_vault_sdk::{CardDetails, PaymentGateway};
-    ///
-    ///
-    /// #[tokio::test]
-    /// async fn create_payment_token() {
-    ///    let card_details = CardDetails {
-    ///        number: NUMBER.to_string(),
-    ///         expYear: EXP_YEAR.to_string(),
-    ///         expMonth: EXP_MONTH.to_string(),
-    ///         cvc: CVC.to_string(),
-    ///     };
-    ///
-    ///     let maya_client = MayaClient::new(
-    ///         env!("ACCESS_TOKEN").to_string(),
-    ///         env!("SECRET_TOKEN").to_string(),
-    ///         None,
-    ///     );
-    ///
-    ///     let res = maya_client
-    ///         .create_payment_token(card_details)
-    ///         .await
-    ///         .unwrap();
-    ///
-    ///     let status_code = res.status();
-    ///
-    ///     assert_eq!(status_code, 200);
-    /// }
-    ///
-    ///
-    /// ```
-    async fn create_payment_token(
-        &self,
-        card_details: CardDetails,
-    ) -> Result<Response, Box<dyn std::error::Error>> {
-        let url = format!("https://{}/payments/v1/payment-tokens", self.url_domain);
-        let auth = format!("Basic {}", &self.auth_header);
-        let request_body = Card { card: card_details };
-
-        let response = self
-            .client
-            .post(&url)
-            .header(AUTHORIZATION, auth)
-            .header(ACCEPT, "application/json")
-            .header(CONTENT_TYPE, "application/json")
-            .json(&request_body)
-            .send()
-            .await?
-            .error_for_status()?;
-
-        Ok(response)
-    }
-
-    async fn create_payment(&self) -> Result<Response, Box<dyn std::error::Error>> {
-        todo!();
-    }
-}
