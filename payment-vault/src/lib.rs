83e02a5acc4a14f811a04fba108eed3e9ed274f6
norman santiago <nsantiago2719@gmail.com>
2023-12-09 01:26:10 +0000

    refactor: remove unused variable

--- payment-vault/src/lib.rs
+++ payment-vault/src/lib.rs
@@ -1,9 +1,6 @@
 use async_trait::async_trait;
 use maya_client_sdk::MayaClient;
-use reqwest::{
-    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
-    Response,
-};
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 use serde::Serialize;
 
 pub use self::payment::CardDetails;
