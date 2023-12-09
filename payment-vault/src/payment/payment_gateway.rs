c911517899a12f5d0e2a1a05261a8745736b511c
norman santiago <nsantiago2719@gmail.com>
2023-12-09 03:55:34 +0000

    refactor: return Result

--- payment-vault/src/payment/payment_gateway.rs
+++ payment-vault/src/payment/payment_gateway.rs
@@ -80,13 +80,16 @@ impl PaymentGateway for MayaClient {
         let auth = format!("Basic {}", &self.auth_header);
         let request_body = payment;
 
-        self.client
+        let res = self
+            .client
             .post(&url)
             .header(AUTHORIZATION, auth)
             .header(ACCEPT, "application/json")
             .header(CONTENT_TYPE, "application/json")
             .json(&request_body)
             .send()
-            .await
+            .await?;
+
+        Ok(res)
     }
 }
